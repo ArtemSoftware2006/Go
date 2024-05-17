df314854b89056266475806592317b5324d5f9b3
Artem Egorov <artem.egorov.art2006@gmail.com>
2024-05-17 19:59:13 +0300

    feat: added creation main mod for all tasks in dir /tasks

--- Yandex/create_tasks_dir.py
+++ Yandex/create_tasks_dir.py
@@ -1,5 +1,6 @@
 import os
 import click
+import subprocess
 
 @click.command()
 @click.argument('task_count', type=int)
@@ -10,20 +11,27 @@ def main(task_count):
         print(f'Directory {dir_name} already exists')
     else:
         os.mkdir(dir_name)
-    
+        
+    os.chdir(dir_name)   
+     
     for i in range(1, task_count+1):
         task_dir_name = '/task' + str(i)
         task_name = '/task' + str(i)
+        
         if os.path.exists(f'{dir_name}{task_dir_name}'):
             print(f'Direcory {dir_name}{task_dir_name} already exists')
         else: 
-            os.mkdir(f'{dir_name}{task_dir_name}')
+            os.mkdir(f'.{task_dir_name}')
 
         if os.path.exists(f'{dir_name}{task_dir_name}{task_name}.go'):
             print(f'File {dir_name}{task_dir_name}{task_name}.go already exists')
         else:
-            with open(f'{dir_name}{task_dir_name}{task_name}.go', 'w') as task_file:
-                task_file.write(f'package {task_dir_name[1:]}')
+            os.chdir(f'.{task_dir_name}')
+            command = f'go mod init {task_name[1:]}'
+            subprocess.run(command, shell=True)
+            with open(f'{task_name[1:]}.go', 'w') as task_file:
+                task_file.write(f'package main')
+            os.chdir('..')
 
     print('Finish create directorys for ' + str(task_count) + ' tasks')
 
